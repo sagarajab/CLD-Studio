c6904fe5da1e35b7b08e978dbd09add9e950e21f
Sagar Bodkhe <42763555+sagarajab@users.noreply.github.com>
2025-07-29 01:55:35 +0530

    modified:   cld-studio/src/main.jsx

--- cld-studio/src/main.jsx
+++ cld-studio/src/main.jsx
@@ -1,7 +1,12 @@
 import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.json';
 
-Amplify.configure(outputs);
+// Configure Amplify - will be updated with actual config from Amplify Console
+const amplifyConfig = {
+  // This will be replaced with actual config from Amplify Console
+  // For now, this allows the app to build without errors
+};
+
+Amplify.configure(amplifyConfig);
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
